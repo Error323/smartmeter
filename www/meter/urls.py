b7f0c65e50e747dda4dcca6e2748cc8c971c8364
Error323 <folkerthuizinga@gmail.com>
2014-01-02 14:04:04 +0100

    Use highstock

--- www/meter/urls.py
+++ www/meter/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url
 from meter import views
 
 urlpatterns = patterns('',
-  url(r'^$', views.index, name='index')
+  url(r'^power/$', views.index, name='index'),
+  url(r'^power/data/$', views.power, name='power'),
+  url(r'^power/data/(?P<start>\d+)/(?P<end>\d+)/$', views.power, name='power')
 )
--- www/meter/views.py
+++ www/meter/views.py
@@ -1,15 +1,37 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-import datetime
 
-class Data:
-  def __init__(self):
-    self.gasinterval = 24*3600*1000
-    self.gasstart = 1388494750
-    self.gasdata = [10, 3, 2, 6, 3, 7, 9, 0, 3, 5, 8, 1]
-    self.powerinterval = 24*3600*1000
-    self.powerstart = 1388494750
-    self.powerdata = [7, 3, 2, 6, 3, 9, 9, 0, 3, 5, 8, 1]
+import rrdtool
+import json
+
+RRDFILE = '/home/fhuizing/Workspace/smartmeter/data/power.rrd'
+POINTS = 1000
 
 def index(request):
-  data = Data()
-  return render(request, 'meter/index.html', {'data': data})
+  return render(request, 'meter/index.html')
+
+def power(request, start=0, end=0):
+  if start == 0 or end == 0:
+    start = rrdtool.first(RRDFILE)
+    end = rrdtool.last(RRDFILE)
+  else:
+    start = max(int(start)/1000, rrdtool.first(RRDFILE))
+    end = min(int(end)/1000, rrdtool.last(RRDFILE))
+
+  resolution = (end - start) / POINTS
+  start = int(start/float(resolution)) * resolution
+  end = int(end/float(resolution)) * resolution
+  
+  raw = rrdtool.fetch(RRDFILE, 'AVERAGE', 
+                      '-r', str(resolution), 
+                      '-s', str(start), 
+                      '-e', str(end))
+  
+  data = []
+  time = raw[0][0]
+  for i in range(len(raw[2])):
+    time += raw[0][2]
+    if raw[2][i][0] != None:
+      data.append((time*1000, int(round(raw[2][i][0]))))
+
+  return HttpResponse(json.dumps(data))
